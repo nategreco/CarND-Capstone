b1b065ec2ff9173d7c3c38e532a6d25d47d7606a
Virendra Anand <vkanand1976@gmail.com>
2018-04-04 03:58:22 +0000

    Traffic Light Classifier with trained model

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,8 +1,21 @@
+import tensorflow as tf
+import sys
+import time
 from styx_msgs.msg import TrafficLight
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
+        #Load lable files
+        
+        label=[line.rstrip() for line 
+                   in tf.gfile.GFile("retrained_labels.txt")]
+
+        #Load the graph
+        with tf.gfile.FastGFile("retrained_graph.pb", 'rb') as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+            _ = tf.import_graph_def(graph_def, name='')
+          
         pass
 
     def get_classification(self, image):
@@ -15,5 +28,41 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        tl_state = TrafficLight.UNKNOWN
+        image_data = tf.gfile.FastGFile(image, 'rb').read()
+        with tf.Session() as sess:
+            softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+            predictions = sess.run(softmax_tensor,{'DecodeJpeg/contents:0': image_data})
+		      	
+            pred_label = predictions[0].argsort()[-len(predictions[0]):][::-1]
+            print(predictions[0][pred_label])
+            print(pred_label)
+            if(predictions[0][pred_label[0]] < 0.5):
+                print("Classification is not good")
+                tl_state = TrafficLight.UNKNOWN
+            else:
+                if(pred_label[0] == 0):
+                    tl_state = TrafficLight.UNKNOWN
+                    print("Light is Off")
+                elif(pred_label[0] == 1):
+                    print("Light is Green")
+                    tl_state = TrafficLight.GREEN
+                elif(pred_label[0] == 2):
+                    print("Light is Yellow")
+                    tl_state = TrafficLight.YELLOW
+                elif(pred_label[0] == 3):
+                    print("Light is Red")
+                    tl_state = TrafficLight.RED
+
+        return tl_state
+
+#x=time.time()
+#testobject = TLClassifier()
+#print(time.time()-x)
+#y=time.time()
+#testobject.get_classification('testImage.jpg')
+#print(time.time()-y)
+#z=time.time()
+#testobject.get_classification('0r1.jpeg')
+#print(time.time()-z)
+#testobject.get_classification('0r3.jpeg')
